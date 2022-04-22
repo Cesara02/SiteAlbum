e80a51b58381f742e3c2d794b7d957b76a02b4ce
Cesara02 <90842096+Cesara02@users.noreply.github.com>
2022-04-22 16:26:25 +0200

    Update album.php

--- album.php
+++ album.php
@@ -2,17 +2,23 @@
 
 class album {
     private $id_;
+    private $nom_;
     private $artiste_;
     private $genre_;
 
-    public function __construct($id, $artiste, $genre) {
-        $this->id=$id_;
-        $this->artiste=$artiste_;
-        $this->genre=$genre_;
+    public function __construct($id, $nom, $artiste, $genre) {
+        $this->id_ = $id;
+        $this->nom_ = $nom;
+        $this->artiste_ = $artiste;
+        $this->genre_ = $genre;
     }
 
     public function getId(){
         return $this->id_;
+    }
+
+    public function getNom(){
+        return $this->nom_;
     }
 
     public function getArtiste(){
